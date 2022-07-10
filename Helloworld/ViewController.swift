634cf281e9e14a6b033cca22987a90db399f6f3a
heesu40 <heesu40@hanmail.net>
2022-07-10 13:20:23 +0900

    Start IOS

--- Helloworld/ViewController.swift
+++ Helloworld/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var IbHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnSend(_ sender: UIButton) {
+        IbHello.text = "Hello, " + txtName.text! 
+    }
 }
 
